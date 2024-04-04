ae09545be83aee22f4cf8ad44657ecf747aa81d2
Md Rashed Hossain <wprashed@icloud.com>
2024-04-04 11:57:15 +0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,23 +1,24 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
--- http://www.phpmyadmin.net
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 01, 2017 at 05:42 AM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Host: localhost:8889
+-- Generation Time: Apr 04, 2024 at 05:56 AM
+-- Server version: 5.7.39
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `testing2`
+-- Database: `inventory`
 --
 
 -- --------------------------------------------------------
@@ -26,19 +27,19 @@ SET time_zone = "+00:00";
 -- Table structure for table `brand`
 --
 
-CREATE TABLE IF NOT EXISTS `brand` (
+CREATE TABLE `brand` (
   `brand_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `brand_name` varchar(250) NOT NULL,
   `brand_status` enum('active','inactive') NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `brand`
 --
 
 INSERT INTO `brand` (`brand_id`, `category_id`, `brand_name`, `brand_status`) VALUES
-(1, 1, 'Finibus', 'active'),
+(1, 1, 'Finibus', 'inactive'),
 (2, 1, 'Lorem', 'active'),
 (3, 1, 'Ipsum', 'active'),
 (4, 8, 'Dolor', 'active'),
@@ -68,11 +69,11 @@ INSERT INTO `brand` (`brand_id`, `category_id`, `brand_name`, `brand_status`) VA
 -- Table structure for table `category`
 --
 
-CREATE TABLE IF NOT EXISTS `category` (
+CREATE TABLE `category` (
   `category_id` int(11) NOT NULL,
   `category_name` varchar(250) NOT NULL,
   `category_status` enum('active','inactive') NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `category`
@@ -99,7 +100,7 @@ INSERT INTO `category` (`category_id`, `category_name`, `category_status`) VALUE
 -- Table structure for table `inventory_order`
 --
 
-CREATE TABLE IF NOT EXISTS `inventory_order` (
+CREATE TABLE `inventory_order` (
   `inventory_order_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `inventory_order_total` double(10,2) NOT NULL,
@@ -109,7 +110,7 @@ CREATE TABLE IF NOT EXISTS `inventory_order` (
   `payment_status` enum('cash','credit') NOT NULL,
   `inventory_order_status` varchar(100) NOT NULL,
   `inventory_order_created_date` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `inventory_order`
@@ -132,8 +133,11 @@ INSERT INTO `inventory_order` (`inventory_order_id`, `user_id`, `inventory_order
 (14, 2, 1196.35, '2017-11-08', 'Mildred Paige', '3167 Oakway Lane\r\nReseda, CA 91335', 'cash', 'active', '2017-11-08'),
 (15, 2, 1960.00, '2017-11-08', 'Elva Lott', '4032 Aaron Smith Drive\r\nHarrisburg, PA 17111', 'credit', 'active', '2017-11-08'),
 (16, 2, 2700.00, '2017-11-08', 'Eric Johnson', '616 Devils Hill Road\r\nJackson, MS 39213', 'cash', 'active', '2017-11-08'),
-(17, 1, 5615.20, '2017-11-09', 'Doris Oliver', '2992 Sycamore Fork Road Hopkins, MN 55343', 'cash', 'active', '2017-11-09'),
-(26, 1, 2278.50, '2017-11-27', 'Janet Richardsons', '4799 Ryder Avenue Everett, WA 98210', 'credit', 'inactive', '2017-11-27');
+(17, 1, 5615.20, '2017-11-09', 'Doris Oliver', '2992 Sycamore Fork Road Hopkins, MN 55343', 'cash', 'inactive', '2017-11-09'),
+(26, 1, 2278.50, '2017-11-27', 'Janet Richardsons', '4799 Ryder Avenue Everett, WA 98210', 'credit', 'inactive', '2017-11-27'),
+(27, 1, 1475.00, '2024-03-22', 'One', 'KJNkjsefs', 'credit', 'active', '2024-03-21'),
+(28, 1, 1132.25, '2024-03-26', 'jbhjkbk', 'n,jnj', 'credit', 'inactive', '2024-03-22'),
+(29, 1, 571.20, '2024-03-25', 'One', 'sfjkskfs', 'cash', 'active', '2024-03-25');
 
 -- --------------------------------------------------------
 
@@ -141,14 +145,14 @@ INSERT INTO `inventory_order` (`inventory_order_id`, `user_id`, `inventory_order
 -- Table structure for table `inventory_order_product`
 --
 
-CREATE TABLE IF NOT EXISTS `inventory_order_product` (
+CREATE TABLE `inventory_order_product` (
   `inventory_order_product_id` int(11) NOT NULL,
   `inventory_order_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
   `price` double(10,2) NOT NULL,
   `tax` double(10,2) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `inventory_order_product`
@@ -204,7 +208,13 @@ INSERT INTO `inventory_order_product` (`inventory_order_product_id`, `inventory_
 (57, 25, 14, 5, 250.00, 18.00),
 (58, 25, 11, 5, 85.00, 12.00),
 (79, 26, 16, 6, 175.00, 5.00),
-(80, 26, 7, 5, 210.00, 12.00);
+(80, 26, 7, 5, 210.00, 12.00),
+(81, 27, 14, 1, 250.00, 18.00),
+(82, 27, 14, 4, 250.00, 18.00),
+(83, 28, 15, 5, 125.00, 5.00),
+(84, 28, 11, 5, 85.00, 12.00),
+(85, 29, 11, 3, 85.00, 12.00),
+(86, 29, 11, 3, 85.00, 12.00);
 
 -- --------------------------------------------------------
 
@@ -212,7 +222,7 @@ INSERT INTO `inventory_order_product` (`inventory_order_product_id`, `inventory_
 -- Table structure for table `product`
 --
 
-CREATE TABLE IF NOT EXISTS `product` (
+CREATE TABLE `product` (
   `product_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `brand_id` int(11) NOT NULL,
@@ -226,7 +236,7 @@ CREATE TABLE IF NOT EXISTS `product` (
   `product_enter_by` int(11) NOT NULL,
   `product_status` enum('active','inactive') NOT NULL,
   `product_date` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product`
@@ -237,8 +247,8 @@ INSERT INTO `product` (`product_id`, `category_id`, `brand_id`, `product_name`, 
 (2, 1, 3, '17W B22 LED Bulb', 'Item Height	14.2 (cm)\r\nColor Temperature (Kelvin)	6500\r\nItem Weight	0.19 (kg)\r\nBulb Material	Aluminium\r\nBase Color	Aluminium\r\nVoltage	240\r\nUsages	Household, Commercial, Kitchen', 150, 'Nos', 350.00, '12.00', 0.00, 1, 'active', '2017-11-08'),
 (3, 8, 5, '18W LED Ceiling Light', 'Round Ceiling Light 18w', 75, 'Nos', 800.00, '5.00', 0.00, 1, 'active', '2017-11-08'),
 (4, 8, 4, 'Round LED Ceiling Light', 'Relying on our expertise in this domain, we are into offering Round LED Ceiling Light.	', 50, 'Nos', 550.00, '12.00', 0.00, 1, 'active', '2017-11-08'),
-(5, 6, 6, '7W LED Concealed Light', 'Dimension ''3" ''\r\n50000 hours burning life\r\ncost effective\r\nhigh quality led', 85, 'Nos', 240.00, '15.00', 0.00, 1, 'active', '2017-11-08'),
-(6, 6, 7, '9w LED Concealed Light', 'dimension ''3" ''\r\n50000 hours burning life\r\ncost effective\r\nhigh quality led', 65, 'Nos', 250.00, '15.00', 0.00, 1, 'active', '2017-11-08'),
+(5, 6, 6, '7W LED Concealed Light', 'Dimension \'3\" \'\r\n50000 hours burning life\r\ncost effective\r\nhigh quality led', 85, 'Nos', 240.00, '15.00', 0.00, 1, 'active', '2017-11-08'),
+(6, 6, 7, '9w LED Concealed Light', 'dimension \'3\" \'\r\n50000 hours burning life\r\ncost effective\r\nhigh quality led', 65, 'Nos', 250.00, '15.00', 0.00, 1, 'active', '2017-11-08'),
 (7, 10, 9, '24W Street Light Led Driver', 'Dc Voltage	36v\r\nRated Current	600ma\r\nRated Power	22w', 120, 'Nos', 210.00, '12.00', 0.00, 1, 'active', '2017-11-08'),
 (8, 10, 8, 'BP1601 ICs', 'Backed by immense industry-experience & latest designing techniques, we are engaged in providing BP1601 ICs.', 200, 'Nos', 15.00, '18.00', 0.00, 1, 'active', '2017-11-08'),
 (9, 3, 11, '5W LED Square Downlight', 'Wattage: 5 Watt\r\nInput Voltage: 150V to 265V, 50/60Hz\r\nLumens: 500 lumen (approx)\r\nPower Factor: 0.90pf', 50, 'Nos', 400.00, '12.00', 0.00, 1, 'active', '2017-11-08'),
@@ -262,28 +272,30 @@ INSERT INTO `product` (`product_id`, `category_id`, `brand_id`, `product_name`, 
 -- Table structure for table `user_details`
 --
 
-CREATE TABLE IF NOT EXISTS `user_details` (
+CREATE TABLE `user_details` (
   `user_id` int(11) NOT NULL,
   `user_email` varchar(200) NOT NULL,
   `user_password` varchar(200) NOT NULL,
-  `user_name` varchar(200) NOT NULL,
+  `user_name` tinytext NOT NULL,
   `user_type` enum('master','user') NOT NULL,
   `user_status` enum('Active','Inactive') NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_details`
 --
 
 INSERT INTO `user_details` (`user_id`, `user_email`, `user_password`, `user_name`, `user_type`, `user_status`) VALUES
-(1, 'john_smith@gmail.com', '$2y$10$0Yo2F.EetL3yhB8l6MNvcOH8AYNS0SuXLOoAQr1qXJa3uPASWV0NC', 'John Smith', 'master', 'Active'),
-(2, 'donahuber@gmail.com', '$2y$10$Ex.JTn6Cu7lCHgaJoCBVeu0qer0cFp2X1qTIpPg2qfIkkuj3LjjMK', 'Dona L. Huber', 'user', 'Active'),
-(3, 'roy_hise@gmail.com', '$2y$10$XlyVI9an5B6rHW3SS9vQpesJssKJxzMQYPbSaR7dnpWjDI5fpxJSS', 'Roy Hise', 'user', 'Active'),
-(4, 'peter_goad@gmail.com', '$2y$10$n1B.FdHNwufTkmzp/pNqc.EiwjB8quQ1tBCEC7nkaldI5pS.et04e', 'Peter Goad', 'user', 'Active'),
-(5, 'sarah_thomas@gmail.com', '$2y$10$s57SErOPlgkIZf1lxzlX3.hMt8LSSKaYig5rusxghDm7LW8RtQc/W', 'Sarah Thomas', 'user', 'Active'),
-(6, 'edna_william@gmail.com', '$2y$10$mfMXnH.TCmg5tlYRhqjxu.ILly8s9.qsLKOpyxgUl6h1fZt6x/B5C', 'Edna William', 'user', 'Active'),
-(8, 'john_parks@gmail.com', '$2y$10$WtsZUxIIz/N4NoIW0Db.pu0VfLWcPs6TyQ8SkpVHLDLGhdNOfALC.', 'John Park', 'user', 'Active'),
-(10, 'peter_parker@gmail.com', '$2y$10$GoQvEZNTWEibo0FPK7h57eA5UsNkXfIdex1deGsW/CFIY8zqxyu2S', 'Mark Parker', 'user', 'Active');
+(1, 'john_smith@gmail.com', '$2y$10$rhXgcuqxup8.un6GzQm1gO5N2eI2/AIcPYGVITg3LEbW9.3LDIlHK', 'John', 'master', 'Active'),
+(2, 'donahuber@gmail.com', '$2y$10$Ex.JTn6Cu7lCHgaJoCBVeu0qer0cFp2X1qTIpPg2qfIkkuj3LjjMK', '0', 'user', 'Active'),
+(3, 'roy_hise@gmail.com', '$2y$10$XlyVI9an5B6rHW3SS9vQpesJssKJxzMQYPbSaR7dnpWjDI5fpxJSS', '0', 'user', 'Active'),
+(4, 'peter_goad@gmail.com', '$2y$10$n1B.FdHNwufTkmzp/pNqc.EiwjB8quQ1tBCEC7nkaldI5pS.et04e', '0', 'user', 'Active'),
+(5, 'sarah_thomas@gmail.com', '$2y$10$s57SErOPlgkIZf1lxzlX3.hMt8LSSKaYig5rusxghDm7LW8RtQc/W', '0', 'user', 'Active'),
+(6, 'edna_william@gmail.com', '$2y$10$mfMXnH.TCmg5tlYRhqjxu.ILly8s9.qsLKOpyxgUl6h1fZt6x/B5C', '0', 'user', 'Active'),
+(8, 'john_parks@gmail.com', '$2y$10$WtsZUxIIz/N4NoIW0Db.pu0VfLWcPs6TyQ8SkpVHLDLGhdNOfALC.', '0', 'user', 'Active'),
+(10, 'peter_parker@gmail.com', '$2y$10$GoQvEZNTWEibo0FPK7h57eA5UsNkXfIdex1deGsW/CFIY8zqxyu2S', '0', 'user', 'Inactive'),
+(11, 'rashedcse18@.com', '$2y$10$89MPuoGhFU8StnTkFzARCulxXjNQRlIYm1TMCTS0HfX7tC1eWxyZ6', '0', 'user', 'Active'),
+(12, 'rashedcse18@gmail.com', '$2y$10$09eL3L21VPgnDOPPVUvXVeRLvp2oaPwz..pRV2PH7whXV6pN0ihfK', '0', 'user', 'Active');
 
 --
 -- Indexes for dumped tables
@@ -333,32 +345,39 @@ ALTER TABLE `user_details`
 -- AUTO_INCREMENT for table `brand`
 --
 ALTER TABLE `brand`
-  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
 --
 -- AUTO_INCREMENT for table `inventory_order`
 --
 ALTER TABLE `inventory_order`
-  MODIFY `inventory_order_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
+  MODIFY `inventory_order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
 --
 -- AUTO_INCREMENT for table `inventory_order_product`
 --
 ALTER TABLE `inventory_order_product`
-  MODIFY `inventory_order_product_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=81;
+  MODIFY `inventory_order_product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
+
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
 --
 -- AUTO_INCREMENT for table `user_details`
 --
 ALTER TABLE `user_details`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
